4641a380a72b8e24b28e066a0b3e566c63105c01
thandiwemagale <80025323+thandiwemagale@users.noreply.github.com>
2021-05-27 11:04:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php 
+extract($_POST);
+if(isset($save))
+{
+	switch($ch)
+	{
+		case '+':
+		$res=$fn+$sn;
+		break;
+		
+		case '-':
+		$res=$fn-$sn;
+		break;
+		
+		case '*':
+		$res=$fn*$sn;
+		break;
+		
+		case '/':
+		$res = $sn == 0 ?$res='cannot divide by zero': $res=$fn/$sn;
+		break;
+	}
+
+}
+
+?>
+<!DOCTYP html>
+<html>
+	<head>
+		<title>Calculator- switch</title>
+		
+	</head>
+	<body>
+		<form method="post">
+		<table border="1" align="center">
+			<tr>
+				<th>Your Result</th>
+				<th><input type="number" readonly="readonly" disabled="disabled" value="<?php  echo $res;?>"/></th>
+			</tr> 
+			
+			<tr>
+				<th>Enter your First num</th>
+				<th><input type="number" name="fn" value="<?php  echo $fn;?>"/></th>
+			</tr> 
+			<tr>
+				<th>Enter your Second num</th>
+				<th><input type="number" name="sn" value="<?php  echo $sn;?>"/></th>
+			</tr>
+			<tr>
+				<th>Select Your Choice</th>
+				<th>
+				<select name="ch">
+					<option>+</option>
+					<option>-</option>
+					<option>*</option>
+                    <?php if($ch == '/') { echo '<option select>/</option>'; }else { echo '<option select>/</option>'; } ?>  
+				</select>
+				</th>
+			</tr>
+			<tr>
+				
+				<th colspan="2">
+				<input type="submit" 
+				name="save" value="Show Result"/>
+				</th>
+			</tr>
+		</table>
+		</form>
+	</body>
+</html>
